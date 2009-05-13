6f2e48f49fd75f0aa841a90601bddb0d949e14bd
Thibaut Barrère <thibaut.barrere@gmail.com>
2009-05-13 14:10:35 +0200

    First public commit

---
+++ labs/plugins/jrubyproxy/Delay.rb
@@ -0,0 +1,206 @@
+require 'java'
+
+# untested
+
+    class Delay < Java::JvstWrapper::VSTPluginAdapter
+      P_DELAY_TIME = 0
+      P_FEEDBACK = 1
+      P_LFO_FREQUENCY = 2
+      P_LFO_DEPTH = 3
+      P_WET_DRY_MIX = 4
+
+      NUM_PARAMS = P_WET_DRY_MIX + 1
+      PARAM_NAMES     = ["Delay Time","Feedback","LFO Frq","LFO Depth","Wet/Dry Mix"]
+      PARAM_LABELS     = ["ms","%","Hz","","%"]
+      PARAM_PRINT_MUL = [1000,100,1,1,100]
+
+      PROGRAMS = [[0.45, 0.50, 0.0, 0, 0.5],
+      [0.01, 0.85, 0.2, 0.5, 0.65],
+      [0.99, 0.7, 0.0, 0.02, 0.50],
+      [0.3, 0.9, 0.0, 0.0, 0.50],
+      [0.004, 0.80, 0.1, 0.8, 0.50],
+      [0.4, 0.50, 0.1, 0.5, 0.50],
+      [0.1, 0.50, 0.1, 0.6, 0.50],
+      [0.1, 0.50, 0.1, 0.7, 0.50]]
+
+      def initialize(wrapper)
+        super
+
+        @echo = []
+        @echoSize = 0
+        @echoPos = 0
+        @echoLFODiff = 0
+        @echoLFODiffMax = 0
+        @echoLFODepth = 0.8
+        @echoFeedback = 0
+        @echoLFOSpeed = 0
+        @echoLFOPos = 0
+        @echoDW = 0.8
+        @sampleRate = 44100    
+        @currentProgramIndex = 0
+
+        update
+
+        setNumInputs(1)
+        setNumOutputs(1)
+        canProcessReplacing(true)
+        setUniqueID(9876543)
+      end
+
+      def setSampleRate(sampleRate)
+        @sampleRate = sampleRate
+      end
+
+      def setEchoTime(millisDelay)
+        @echoSize = millisDelay * @sampleRate / 1000
+        @echo = Array.new(@echoSize) unless @echo.size == @echoSize
+      end
+
+      def currentProgram
+        PROGRAMS[@currentProgramIndex]
+      end
+
+      def update
+        setEchoTime(currentProgram[P_DELAY_TIME] * 1000)
+        @echoFeedback = currentProgram[P_FEEDBACK]
+        @echoLFOSpeed = currentProgram[P_LFO_FREQUENCY] * 2 * 3.1415 / @sampleRate
+        @echoLFODepth = currentProgram[P_LFO_DEPTH]
+        @echoLFODiffMax = (@echoSize / 2.0) * @echoLFODepth
+        @echoLFODiff = 0
+        @echoDW = currentProgram[P_WET_DRY_MIX]
+        @echoPos = 0
+      end
+
+      def canDo(feature)
+        ret = CANDO_NO
+        ret = CANDO_YES if feature == CANDO_PLUG_1_IN_1_OUT
+        ret = CANDO_YES if feature == CANDO_PLUG_PLUG_AS_CHANNEL_INSERT
+        ret = CANDO_YES if feature == CANDO_PLUG_PLUG_AS_SEND
+
+        log("canDo: #{feature} = #{ret}")
+        ret
+      end
+
+      def getProductString
+        "Opaz"
+      end
+
+      def getEffectname
+        "Delay"
+      end
+
+      def getProgramNameIndexed(category,index)
+        "Prog: cat #{category}, #{index}"
+      end
+
+      def getVendorString
+        "LoGeek"
+      end
+
+      def setBypass(value)
+        false
+      end
+
+      def string2Parameter(index,value)
+        begin
+          setParameter(index, Float(value)) unless value.nil?
+          return true
+        rescue
+          return false
+        end
+      end
+
+      def getNumParams
+        NUM_PARAMS
+      end
+
+      def getNumPrograms
+        PROGRAMS.size
+      end
+
+      def getParameter(index)
+        if index < currentProgram.size
+          return currentProgram[index]
+        else
+          return 0.0
+        end
+      end
+
+      def getParameterDisplay(index)
+        if index < currentProgram.size
+          return ((100 * PARAM_PRINT_MUL[index] * currentProgram[index]) / 100.0).to_s
+        else
+          return "0.0"
+        end
+      end
+
+      def getParameterLabel(index)
+        if index < currentProgram.size
+          return PARAM_LABELS[index]
+        else
+          return ""
+        end
+      end
+
+      def getParameterName(index)
+        if index < currentProgram.size
+          return PARAM_NAMES[index]
+        else
+          return "param: #{index}"
+        end
+      end
+
+      def getProgram
+        @currentProgramIndex
+      end
+
+      def getProgramName
+        "program #{@currentProgramIndex}"
+      end
+
+      def setParameter(index,value)
+        currentProgram[index] = value
+        update
+      end
+
+      def setProgram(index)
+        @currentProgramIndex = index
+        update
+      end
+
+      def setProgramName(name)
+        #ignore
+      end
+
+      def getPlugCategory
+        log("getPlugCategory")
+        PLUG_CATEG_EFFECT
+      end
+
+      def processReplacing(inputs, outputs, sampleFrames)
+        inBuffer = inputs[0]
+        outBuffer = outputs[0]
+        for i in (0..sampleFrames-1)
+          exVal = inBuffer[i]
+          echoRead = @echoPos + @echoLFODiff
+          if (echoRead >= @echoSize)
+            echoRead -= @echoSize
+          end
+
+          out = (exVal * (1.0 - @echoDW) + @echo[echoRead] * echoDW)
+          outBuffer[i] = out
+
+          exVal = exVal + @echo[echoRead] * @echoFeedback
+
+          @echo[@echoPos] = exVal
+          @echoPos += 1
+          if (@echoPos >= @echoSize)
+            @echoPos = 0
+          end
+        end
+
+        @echoLFODiff = @echoLFODiff * (1.0 + Math.sin(@echoLFOPos))
+        @echoLFOPos += @echoLFOSpeed * sampleFrames
+      end
+
+    end
---
+++ labs/plugins/jrubyproxy/JRubyProxy.java
@@ -0,0 +1,146 @@
+import jvst.wrapper.*;
+import org.jruby.Ruby;
+import org.jruby.runtime.builtin.*;
+import org.jruby.javasupport.JavaEmbedUtils;
+import java.lang.reflect.*;
+
+public class JRubyProxy extends VSTPluginAdapter {
+
+	public JRubyProxy(long wrapper) {
+		super(wrapper);
+
+		log("***************** boot *****************");
+		Ruby runtime = Ruby.getDefaultInstance();
+		
+		String rubySourceFile = "/Users/thbar/git/opaz/labs/eclipse-sandbox/Gain/ruby_src/Delay.rb";
+		String rubyCode = runtime.evalScriptlet("File.open('"+ rubySourceFile+"').read").toString();
+		runtime.evalScriptlet(rubyCode);
+		
+		Object rfj = runtime.evalScriptlet("Delay.new("+wrapper+")");
+		VSTPluginAdapter adapter = (VSTPluginAdapter)JavaEmbedUtils.rubyToJava(runtime, (IRubyObject) rfj, VSTPluginAdapter.class);
+
+		//log("adapter.getClass() => " + adapter.getClass());
+
+ 	Method m[] = adapter.getClass().getMethods();
+	for (int i = 0; i < m.length; i++)
+		System.out.println(m[i].toString());
+		
+		/*
+		log("---- adapter.getClass().getDeclaredMethods() ----");
+		Method m[] = adapter.getClass().getDeclaredMethods();
+		for (int i = 0; i < m.length; i++)
+			System.out.println(m[i].toString());
+		
+		log("---- adapter.getClass().getDeclaredMethods() ----");
+		Field f[] = adapter.getClass().getFields();
+		for (int i = 0; i < f.length; i++)
+			System.out.println(f[i].toString());
+		*/
+		
+		// communicate with the host
+		this.setNumInputs(1);// mono input
+		this.setNumOutputs(1);// mono output
+		// this.hasVu(false); //deprecated as of vst2.4
+		this.canProcessReplacing(true);// mandatory for vst 2.4!
+		this.setUniqueID(9876557);// random unique number registered at
+									// steinberg (4 byte)
+
+		this.canMono(true);
+		log("Exiting constructor...");
+	}
+
+	public int canDo(String arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getPlugCategory() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getProductString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getProgramNameIndexed(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getVendorString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean setBypass(boolean arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean string2Parameter(int arg0, String arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int getNumParams() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getNumPrograms() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public float getParameter(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getParameterDisplay(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getParameterLabel(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getParameterName(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getProgram() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getProgramName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void processReplacing(float[][] arg0, float[][] arg1, int arg2) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setParameter(int arg0, float arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setProgram(int arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setProgramName(String arg0) {
+		// TODO Auto-generated method stub
+	}
+
+}
