980738b94cefef080bacfcf9de93e80832bcb8f9
Thibaut Barrère <thibaut.barrere@gmail.com>
2009-05-18 23:32:45 +0200

    Added missing changes from previous patch

--- plugins/jrubyproxy/JRubyVSTPluginProxy.java
+++ plugins/jrubyproxy/JRubyVSTPluginProxy.java
@@ -1,147 +1,131 @@
-import jvst.wrapper.*;
+package loader;
+import jvst.wrapper.VSTPluginAdapter;
+
 import org.jruby.Ruby;
-import org.jruby.runtime.builtin.*;
 import org.jruby.javasupport.JavaEmbedUtils;
-import java.lang.reflect.*;
+import org.jruby.runtime.builtin.IRubyObject;
 
-public class JRubyProxy extends VSTPluginAdapter {
+public class JRubyVSTPluginProxy extends VSTPluginAdapter {
 
-	public JRubyProxy(long wrapper) {
+	protected VSTPluginAdapter adapter;
+	
+	public JRubyVSTPluginProxy(long wrapper) {
 		super(wrapper);
 
+		// TODO: redirect stdin, out and err to a file 
 		log("***************** boot *****************");
 		Ruby runtime = Ruby.getDefaultInstance();
 		
 		// TODO - load relatively to the loader folder
-		String rubySourceFile = "/Users/thbar/git/opaz/labs/eclipse-sandbox/Gain/ruby_src/Delay.rb";
+		// TODO: make configurable (jvstruby.ini or similar)
+		//String rubySourceFile = "/Users/thbar/git/opaz/labs/eclipse-sandbox/Gain/ruby_src/Delay.rb";
+		String rubySourceFile = "C:/@DATA/private/jvst/opaz-plugdk/src/Delay.rb";
 		String rubyCode = runtime.evalScriptlet("File.open('"+ rubySourceFile+"').read").toString();
 		runtime.evalScriptlet(rubyCode);
 		
 		Object rfj = runtime.evalScriptlet("Delay.new("+wrapper+")");
-		VSTPluginAdapter adapter = (VSTPluginAdapter)JavaEmbedUtils.rubyToJava(runtime, (IRubyObject) rfj, VSTPluginAdapter.class);
-
-		//log("adapter.getClass() => " + adapter.getClass());
-
- 	Method m[] = adapter.getClass().getMethods();
-	for (int i = 0; i < m.length; i++)
-		System.out.println(m[i].toString());
+		this.adapter = (VSTPluginAdapter)JavaEmbedUtils.rubyToJava(runtime, (IRubyObject) rfj, VSTPluginAdapter.class);
 		
-		/*
-		log("---- adapter.getClass().getDeclaredMethods() ----");
-		Method m[] = adapter.getClass().getDeclaredMethods();
-		for (int i = 0; i < m.length; i++)
-			System.out.println(m[i].toString());
-		
-		log("---- adapter.getClass().getDeclaredMethods() ----");
-		Field f[] = adapter.getClass().getFields();
-		for (int i = 0; i < f.length; i++)
-			System.out.println(f[i].toString());
-		*/
-		
-		// communicate with the host
-		this.setNumInputs(1);// mono input
-		this.setNumOutputs(1);// mono output
-		// this.hasVu(false); //deprecated as of vst2.4
-		this.canProcessReplacing(true);// mandatory for vst 2.4!
-		this.setUniqueID(9876557);// random unique number registered at
-									// steinberg (4 byte)
-
-		this.canMono(true);
 		log("Exiting constructor...");
 	}
+	
+	//convenience for logging in jruby
+	public void Log(String msg) {
+		log(msg);
+	}
 
+	@Override
 	public int canDo(String arg0) {
-		// TODO Auto-generated method stub
-		return 0;
+		return adapter.canDo(arg0);
 	}
 
+	@Override
 	public int getPlugCategory() {
-		// TODO Auto-generated method stub
-		return 0;
+		return adapter.getPlugCategory();
 	}
 
+	@Override
 	public String getProductString() {
-		// TODO Auto-generated method stub
-		return null;
+		return adapter.getProductString();
 	}
 
+	@Override
 	public String getProgramNameIndexed(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return null;
+		return adapter.getProgramName();
 	}
 
+	@Override
 	public String getVendorString() {
-		// TODO Auto-generated method stub
-		return null;
+		return adapter.getVendorString();
 	}
-
+	
+	@Override
 	public boolean setBypass(boolean arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		return adapter.setBypass(arg0);
 	}
 
+	@Override
 	public boolean string2Parameter(int arg0, String arg1) {
-		// TODO Auto-generated method stub
-		return false;
+		return adapter.string2Parameter(arg0, arg1);
 	}
 
+	@Override
 	public int getNumParams() {
-		// TODO Auto-generated method stub
-		return 0;
+		return adapter.getNumParams();
 	}
 
+	@Override
 	public int getNumPrograms() {
-		// TODO Auto-generated method stub
-		return 0;
+		return adapter.getNumPrograms();
 	}
 
+	@Override
 	public float getParameter(int arg0) {
-		// TODO Auto-generated method stub
-		return 0;
+		return adapter.getParameter(arg0);
 	}
 
+	@Override
 	public String getParameterDisplay(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		return adapter.getParameterDisplay(arg0);
 	}
 
+	@Override
 	public String getParameterLabel(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		return adapter.getParameterLabel(arg0);
 	}
 
+	@Override
 	public String getParameterName(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		return adapter.getParameterName(arg0);
 	}
 
+	@Override
 	public int getProgram() {
-		// TODO Auto-generated method stub
-		return 0;
+		return adapter.getProgram();
 	}
 
+	@Override
 	public String getProgramName() {
-		// TODO Auto-generated method stub
-		return null;
+		return adapter.getProgramName();
 	}
 
+	@Override
 	public void processReplacing(float[][] arg0, float[][] arg1, int arg2) {
-		// TODO Auto-generated method stub
-
+		adapter.processReplacing(arg0, arg1, arg2);
 	}
 
+	@Override
 	public void setParameter(int arg0, float arg1) {
-		// TODO Auto-generated method stub
-
+		adapter.setParameter(arg0, arg1);
 	}
 
+	@Override
 	public void setProgram(int arg0) {
-		// TODO Auto-generated method stub
-
+		adapter.setProgram(arg0);
 	}
 
+	@Override
 	public void setProgramName(String arg0) {
-		// TODO Auto-generated method stub
+		adapter.setProgramName(arg0);
 	}
-
 }
