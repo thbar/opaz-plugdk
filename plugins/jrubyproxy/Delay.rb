5ba7bf0177f83e940c226a6a509b9984f93d2ff0
Thibaut Barrère <thibaut.barrere@gmail.com>
2009-05-18 23:20:08 +0200

    Pushing back working plugin (patch by Daniel Martin)

--- plugins/jrubyproxy/Delay.rb
+++ plugins/jrubyproxy/Delay.rb
@@ -1,206 +1,219 @@
 require 'java'
 
-# untested
+# this is to be able to call the static log() method in VSTPluginAdapter
+include_class 'jvst.wrapper.VSTPluginAdapter'
 
-    class Delay < Java::JvstWrapper::VSTPluginAdapter
-      P_DELAY_TIME = 0
-      P_FEEDBACK = 1
-      P_LFO_FREQUENCY = 2
-      P_LFO_DEPTH = 3
-      P_WET_DRY_MIX = 4
+class Delay < Java::jvst.wrapper.VSTPluginAdapter
+  P_DELAY_TIME = 0
+  P_FEEDBACK = 1
+  P_LFO_FREQUENCY = 2
+  P_LFO_DEPTH = 3
+  P_WET_DRY_MIX = 4
 
-      NUM_PARAMS = P_WET_DRY_MIX + 1
-      PARAM_NAMES     = ["Delay Time","Feedback","LFO Frq","LFO Depth","Wet/Dry Mix"]
-      PARAM_LABELS     = ["ms","%","Hz","","%"]
-      PARAM_PRINT_MUL = [1000,100,1,1,100]
+  NUM_PARAMS = P_WET_DRY_MIX + 1
+  PARAM_NAMES = ["Delay Time","Feedback","LFO Frq","LFO Depth","Wet/Dry Mix"]
+  PARAM_LABELS = ["ms","%","Hz","","%"]
+  PARAM_PRINT_MUL = [1000,100,1,1,100]
 
-      PROGRAMS = [[0.45, 0.50, 0.0, 0, 0.5],
-      [0.01, 0.85, 0.2, 0.5, 0.65],
-      [0.99, 0.7, 0.0, 0.02, 0.50],
-      [0.3, 0.9, 0.0, 0.0, 0.50],
-      [0.004, 0.80, 0.1, 0.8, 0.50],
-      [0.4, 0.50, 0.1, 0.5, 0.50],
-      [0.1, 0.50, 0.1, 0.6, 0.50],
-      [0.1, 0.50, 0.1, 0.7, 0.50]]
+  PROGRAMS = [[0.45, 0.50, 0.0, 0, 0.5],
+  [0.01, 0.85, 0.2, 0.5, 0.65],
+  [0.99, 0.7, 0.0, 0.02, 0.50],
+  [0.3, 0.9, 0.0, 0.0, 0.50],
+  [0.004, 0.80, 0.1, 0.8, 0.50],
+  [0.4, 0.50, 0.1, 0.5, 0.50],
+  [0.1, 0.50, 0.1, 0.6, 0.50],
+  [0.1, 0.50, 0.1, 0.7, 0.50]]
 
-      def initialize(wrapper)
-        super
+  def initialize(wrapper)
+	super
 
-        @echo = []
-        @echoSize = 0
-        @echoPos = 0
-        @echoLFODiff = 0
-        @echoLFODiffMax = 0
-        @echoLFODepth = 0.8
-        @echoFeedback = 0
-        @echoLFOSpeed = 0
-        @echoLFOPos = 0
-        @echoDW = 0.8
-        @sampleRate = 44100    
-        @currentProgramIndex = 0
+	@echo = []
+	@echoSize = 0
+	@echoPos = 0
+	@echoLFODiff = 0
+	@echoLFODiffMax = 0
+	@echoLFODepth = 0.8
+	@echoFeedback = 0
+	@echoLFOSpeed = 0
+	@echoLFOPos = 0
+	@echoDW = 0.8
+	@sampleRate = 44100
+	@currentProgramIndex = 0
 
-        update
+	update
 
-        setNumInputs(1)
-        setNumOutputs(1)
-        canProcessReplacing(true)
-        setUniqueID(9876543)
-      end
+	setNumInputs(1)
+	setNumOutputs(1)
+	canProcessReplacing(true)
+	setUniqueID(9876543)
+  end
 
-      def setSampleRate(sampleRate)
-        @sampleRate = sampleRate
-      end
+  #convenience
+  def log(msg)
+	VSTPluginAdapter.log("JRuby: #{msg}")
+  end
 
-      def setEchoTime(millisDelay)
-        @echoSize = millisDelay * @sampleRate / 1000
-        @echo = Array.new(@echoSize) unless @echo.size == @echoSize
-      end
+  def setSampleRate(sampleRate)
+	@sampleRate = sampleRate
+  end
 
-      def currentProgram
-        PROGRAMS[@currentProgramIndex]
-      end
+  def setEchoTime(millisDelay)
+	@echoSize = millisDelay * (@sampleRate / 1000)
+	@echo = Array.new(@echoSize) unless @echo.size == @echoSize
+  end
 
-      def update
-        setEchoTime(currentProgram[P_DELAY_TIME] * 1000)
-        @echoFeedback = currentProgram[P_FEEDBACK]
-        @echoLFOSpeed = currentProgram[P_LFO_FREQUENCY] * 2 * 3.1415 / @sampleRate
-        @echoLFODepth = currentProgram[P_LFO_DEPTH]
-        @echoLFODiffMax = (@echoSize / 2.0) * @echoLFODepth
-        @echoLFODiff = 0
-        @echoDW = currentProgram[P_WET_DRY_MIX]
-        @echoPos = 0
-      end
+  def currentProgram
+	PROGRAMS[@currentProgramIndex]
+  end
 
-      def canDo(feature)
-        ret = CANDO_NO
-        ret = CANDO_YES if feature == CANDO_PLUG_1_IN_1_OUT
-        ret = CANDO_YES if feature == CANDO_PLUG_PLUG_AS_CHANNEL_INSERT
-        ret = CANDO_YES if feature == CANDO_PLUG_PLUG_AS_SEND
+  def update
+	setEchoTime(currentProgram[P_DELAY_TIME] * 1000)
+	@echoFeedback = currentProgram[P_FEEDBACK]
+	@echoLFOSpeed = currentProgram[P_LFO_FREQUENCY] * 2 * 3.1415 / @sampleRate
+	@echoLFODepth = currentProgram[P_LFO_DEPTH]
+	@echoLFODiffMax = (@echoSize / 2.0) * @echoLFODepth
+	@echoLFODiff = 0
+	@echoDW = currentProgram[P_WET_DRY_MIX]
+	@echoPos = 0
+  end
 
-        log("canDo: #{feature} = #{ret}")
-        ret
-      end
+  def canDo(feature)
+	ret = CANDO_NO
+	ret = CANDO_YES if feature == CANDO_PLUG_1_IN_1_OUT
+	ret = CANDO_YES if feature == CANDO_PLUG_PLUG_AS_CHANNEL_INSERT
+	ret = CANDO_YES if feature == CANDO_PLUG_PLUG_AS_SEND
 
-      def getProductString
-        "Opaz"
-      end
+	log("canDo: #{feature} = #{ret}")
+	ret
+  end
 
-      def getEffectname
-        "Delay"
-      end
+  def getProductString
+	"Opaz"
+  end
 
-      def getProgramNameIndexed(category,index)
-        "Prog: cat #{category}, #{index}"
-      end
+  def getEffectname
+	"Delay"
+  end
 
-      def getVendorString
-        "LoGeek"
-      end
+  def getProgramNameIndexed(category,index)
+	"Prog: cat #{category}, #{index}"
+  end
 
-      def setBypass(value)
-        false
-      end
+  def getVendorString
+	"LoGeek"
+  end
 
-      def string2Parameter(index,value)
-        begin
-          setParameter(index, Float(value)) unless value.nil?
-          return true
-        rescue
-          return false
-        end
-      end
+  def setBypass(value)
+	false
+  end
 
-      def getNumParams
-        NUM_PARAMS
-      end
+  def string2Parameter(index,value)
+	begin
+	  setParameter(index, Float(value)) unless value.nil?
+	  return true
+	rescue
+	  return false
+	end
+  end
 
-      def getNumPrograms
-        PROGRAMS.size
-      end
+  def getNumParams
+	NUM_PARAMS
+  end
 
-      def getParameter(index)
-        if index < currentProgram.size
-          return currentProgram[index]
-        else
-          return 0.0
-        end
-      end
+  def getNumPrograms
+	PROGRAMS.size
+  end
 
-      def getParameterDisplay(index)
-        if index < currentProgram.size
-          return ((100 * PARAM_PRINT_MUL[index] * currentProgram[index]) / 100.0).to_s
-        else
-          return "0.0"
-        end
-      end
+  def getParameter(index)
+	if index < currentProgram.size
+	  return currentProgram[index]
+	else
+	  return 0.0
+	end
+  end
 
-      def getParameterLabel(index)
-        if index < currentProgram.size
-          return PARAM_LABELS[index]
-        else
-          return ""
-        end
-      end
+  def getParameterDisplay(index)
+	if index < currentProgram.size
+	  return ((100 * PARAM_PRINT_MUL[index] * currentProgram[index]) / 100.0).to_s
+	else
+	  return "0.0"
+	end
+  end
 
-      def getParameterName(index)
-        if index < currentProgram.size
-          return PARAM_NAMES[index]
-        else
-          return "param: #{index}"
-        end
-      end
+  def getParameterLabel(index)
+	if index < currentProgram.size
+	  return PARAM_LABELS[index]
+	else
+	  return ""
+	end
+  end
 
-      def getProgram
-        @currentProgramIndex
-      end
+  def getParameterName(index)
+	if index < currentProgram.size
+	  return PARAM_NAMES[index]
+	else
+	  return "param: #{index}"
+	end
+  end
 
-      def getProgramName
-        "program #{@currentProgramIndex}"
-      end
+  def getProgram
+	@currentProgramIndex
+  end
 
-      def setParameter(index,value)
-        currentProgram[index] = value
-        update
-      end
+  def getProgramName
+	"program #{@currentProgramIndex}"
+  end
 
-      def setProgram(index)
-        @currentProgramIndex = index
-        update
-      end
+  def setParameter(index,value)
+	currentProgram[index] = value
+	update
+  end
 
-      def setProgramName(name)
-        #ignore
-      end
+  def setProgram(index)
+	@currentProgramIndex = index
+	update
+  end
 
-      def getPlugCategory
-        log("getPlugCategory")
-        PLUG_CATEG_EFFECT
-      end
+  def setProgramName(name)
+	#ignore
+  end
 
-      def processReplacing(inputs, outputs, sampleFrames)
-        inBuffer = inputs[0]
-        outBuffer = outputs[0]
-        for i in (0..sampleFrames-1)
-          exVal = inBuffer[i]
-          echoRead = @echoPos + @echoLFODiff
-          if (echoRead >= @echoSize)
-            echoRead -= @echoSize
-          end
+  def getPlugCategory
+	log("getPlugCategory")
+	PLUG_CATEG_EFFECT
+  end
 
-          out = (exVal * (1.0 - @echoDW) + @echo[echoRead] * echoDW)
-          outBuffer[i] = out
+  def processReplacing(inputs, outputs, sampleFrames)
+	inBuffer = inputs[0]
+	outBuffer = outputs[0]
+	for i in (0..sampleFrames-1)
+	  exVal = inBuffer[i]
+	  echoRead = @echoPos + @echoLFODiff
+	  if (echoRead >= @echoSize)
+		echoRead -= @echoSize
+	  end
 
-          exVal = exVal + @echo[echoRead] * @echoFeedback
+	  #log("#{exVal} * (1.0 - #{@echoDW}) + #{@echo[echoRead]} * #{@echoDW}")
+	  #log("echo[#{echoRead}] = #{@echo[echoRead]}")
+	  
+	  #error here: @echo[echoRead] = nil
+	  #out = exVal * (1.0 - @echoDW) + @echo[echoRead] * @echoDW
+	  out = inBuffer[i] #just to hear something
+	  outBuffer[i] = out*@echoDW
 
-          @echo[@echoPos] = exVal
-          @echoPos += 1
-          if (@echoPos >= @echoSize)
-            @echoPos = 0
-          end
-        end
+	  #error here: @echo[echoRead] = nil
+	  #exVal = exVal + @echo[echoRead] * @echoFeedback
+	
+	  @echo[@echoPos] = exVal
+	  @echoPos += 1
+	  if (@echoPos >= @echoSize)
+		@echoPos = 0
+	  end
+	end
 
-        @echoLFODiff = @echoLFODiff * (1.0 + Math.sin(@echoLFOPos))
-        @echoLFOPos += @echoLFOSpeed * sampleFrames
-      end
+	@echoLFODiff = @echoLFODiff * (1.0 + Math.sin(@echoLFOPos))
+	@echoLFOPos += @echoLFOSpeed * sampleFrames
+  end
 
-    end
+end
+ 
