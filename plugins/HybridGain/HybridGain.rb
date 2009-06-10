18b899a651ab8e39e9f07931d528e4d8c0cd9b91
Thibaut Barrère <thibaut.barrere@gmail.com>
2009-06-10 12:44:23 +0200

    Non-working HybridGain plugin (mixes jruby and java)

---
+++ plugins/HybridGain/HybridGain.rb
@@ -0,0 +1,15 @@
+# does not work - cannot load tools class, for some reason
+
+include_class 'HybridGainTools'
+
+class HybridGain < OpazPlug
+  plugin "HybridGain", "Opaz", "LoGeek"
+  can_do "1in1out", "plugAsChannelInsert", "plugAsSend"
+  unique_id 9876549
+  
+  param :gain, "Gain", 1.0
+
+  def process(inputs, outputs, sampleFrames)
+    HybridGainTools.applyGain(inputs[0], outputs[0], sampleFrames, gain)
+  end
+end
