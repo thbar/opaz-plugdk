2824acc9a02482a7c48af9a2ed20a5954570ef21
daniel309 <daniel309@users.sourceforge.net>
2009-06-12 12:06:47 +0200

    minor changes to MockVSTHost. beware, untestet so far

--- src/MockVSTHost.java
+++ src/MockVSTHost.java
@@ -1,18 +1,24 @@
 
 public class MockVSTHost {
 	
+	//TODO: make platform aware and generalize
+	private static final String NATIVE_LIB_LOCATION = "C:/@DATA/private/jvst/jvst_native/src/Release/jvstwrapper.dll";
+	
+	
 	public static void main(String[] args) {
-		//satisfy jvstwrapper native methods
-		//TODO: make platform aware...
-		System.load("C:/@DATA/private/jvst/jvst_native/src/Release/jvstwrapper.dll");
-		
-		//directly instantiate plugin
+		//System.load(NATIVE_LIB_LOCATION);
 		//JRubyVSTPluginProxy.setIsLogEnabled(true);//this only works if using JVSTPluginAdapter.java from the CVS HEAD
-		JRubyVSTPluginProxy p = new JRubyVSTPluginProxy(0);
+													//enables logging to be send directly to stdout instead of a file
 		
-		//NOTE: because 0 being passed as the reference to the native counterpart of the VST plugin should cause all plug-->host calls 
-		//      (e.g. this.setNumInputs(1)) to be ignored. Using any other value than 0 immediately caused crashes since 
-		//      this there is no VST plugin instance at this memory address (see e.g. VSTV10ToHost.cpp)
+		JRubyVSTPluginProxy._initPlugFromNative(NATIVE_LIB_LOCATION, true); //enable logging (this creates the _java_stdout.txt file as usual)
+		JRubyVSTPluginProxy p = new JRubyVSTPluginProxy(0);
+		//NOTE: because 0 passed as the reference to the native counterpart of the VST plugin, all plug-->host calls 
+		//      (e.g. this.setNumInputs(1)) are ignored. Using any other value than 0 immediately causes crashes since 
+		//      there is no VST plugin instance at this address in memory (see VSTV10ToHost.cpp)
+		
+		
+		//TODO: plugin init seqence here (suspend, resume, ...)
+		
 		
 		//process some audio (host --> plug)
 		p.processReplacing(new float[][]{{}}, new float[][]{{}}, 0);
