d82ce1080c73f32f270b29421c0cbace0bb0b48e
Thibaut Barrère <thibaut.barrere@gmail.com>
2009-07-05 00:44:48 +0200

    Removing experimental Clojure stuff from master. Will be kept under branch clojure-tryout.

--- hello-clojure.rb
+++
@@ -1,9 +0,0 @@
-# simple loading test from clojure
-# compile occurs at runtime, which means it would be easier to tweak in some repl style
-# run through jruby!
-
-require 'java'
-require 'libs/clojure-1.0.0.jar'
-require 'src/clojure.rb'
-
-puts Clojure.execute("(+ 1 2)")
